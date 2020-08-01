6ac40298d1a74c7af89d9d0b972a8fef6eb9f246
jordivilagut <vilagutjordi@gmail.com>
2020-08-01 22:58:52 +0200

    Handle successful authentication

--- src/main/kotlin/com/jordivilagut/fintracking/security/UserAuthFilter.kt
+++ src/main/kotlin/com/jordivilagut/fintracking/security/UserAuthFilter.kt
@@ -1,13 +1,19 @@
 package com.jordivilagut.fintracking.security
 
+import com.jordivilagut.fintracking.exceptions.InvalidUserException
 import com.jordivilagut.fintracking.services.UserService
 import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter
 import org.springframework.security.web.util.matcher.RequestMatcher
+import java.io.IOException
+import javax.servlet.FilterChain
+import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+
 
 class UserAuthFilter(
         private val userService: UserService,
@@ -20,5 +26,23 @@ class UserAuthFilter(
         val user = userService.findByToken(token)
         val auth = UsernamePasswordAuthenticationToken(user, token)
         return authenticationManager.authenticate(auth)
+    }
+
+    @Throws(IOException::class, ServletException::class)
+    override fun successfulAuthentication(
+            request: HttpServletRequest,
+            response: HttpServletResponse,
+            chain: FilterChain,
+            authResult: Authentication) {
+
+        super.successfulAuthentication(request, response, chain, authResult)
+        chain.doFilter(request, response)
+    }
+
+    override fun unsuccessfulAuthentication(request: HttpServletRequest,
+                                            response: HttpServletResponse,
+                                            failed: AuthenticationException) {
+
+        throw InvalidUserException("Unsuccessful authentication.")
     }
 }
