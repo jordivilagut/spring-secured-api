03741342791fe15b0ef001cb85f5de1ce6eace29
jordivilagut <vilagutjordi@gmail.com>
2020-08-09 20:25:28 +0200

    Implement REST security with 200 and 401 responses for success and error

--- src/main/kotlin/com/jordivilagut/fintracking/security/SecurityConfig.kt
+++ src/main/kotlin/com/jordivilagut/fintracking/security/SecurityConfig.kt
@@ -1,28 +1,25 @@
 package com.jordivilagut.fintracking.security
 
-import com.jordivilagut.fintracking.services.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.web.authentication.AnonymousAuthenticationFilter
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher
-import org.springframework.security.web.util.matcher.NegatedRequestMatcher
+import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig
 
     constructor(
-        @Autowired val userService: UserService,
-        @Autowired val authProvider: UserAuthProvider)
+        @Autowired val userAuthFilter: UserAuthFilter,
+        @Autowired val authEntryPoint: AuthEntryPoint)
 
     : WebSecurityConfigurerAdapter() {
 
@@ -33,29 +30,29 @@ class SecurityConfig
 
     override fun configure(http: HttpSecurity) {
         http
-                .cors().configurationSource(corsConfigurationSource())
-                .and().authenticationProvider(authProvider)
-                .addFilterBefore(userAuthFilter(), AnonymousAuthenticationFilter::class.java)
+                .cors()
+                    .configurationSource(corsConfigurationSource())
+                    .and()
+                .csrf()
+                    .disable()
                 .authorizeRequests()
                     .antMatchers(LOGIN_URL).permitAll()
                     .anyRequest().authenticated()
-                .and().csrf().disable()
+                    .and()
+                .exceptionHandling()
+                    .authenticationEntryPoint(authEntryPoint)
+                    .and()
+                .sessionManagement()
+                    .sessionCreationPolicy(STATELESS);
+
+        http.addFilterBefore(userAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
     }
+
 
     @Bean
     @Throws(Exception::class)
-    fun userAuthFilter(): UserAuthFilter {
-        val filter = UserAuthFilter(userService, NegatedRequestMatcher(AntPathRequestMatcher(LOGIN_URL)))
-        filter.setAuthenticationManager(authenticationManager())
-        filter.setAuthenticationSuccessHandler(successHandler())
-        return filter
-    }
-
-    @Bean
-    fun successHandler(): SimpleUrlAuthenticationSuccessHandler {
-        val successHandler = SimpleUrlAuthenticationSuccessHandler()
-        successHandler.setRedirectStrategy(NoRedirect())
-        return successHandler
+    override fun authenticationManager(): AuthenticationManager {
+        return super.authenticationManagerBean()
     }
 
     @Bean
--- src/main/kotlin/com/jordivilagut/fintracking/security/UserAuthFilter.kt
+++ src/main/kotlin/com/jordivilagut/fintracking/security/UserAuthFilter.kt
@@ -1,48 +1,58 @@
 package com.jordivilagut.fintracking.security
 
-import com.jordivilagut.fintracking.exceptions.InvalidUserException
+import com.jordivilagut.fintracking.services.TokenService
 import com.jordivilagut.fintracking.services.UserService
-import org.springframework.http.HttpHeaders.AUTHORIZATION
+import io.jsonwebtoken.ExpiredJwtException
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.Authentication
-import org.springframework.security.core.AuthenticationException
-import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter
-import org.springframework.security.web.util.matcher.RequestMatcher
-import java.io.IOException
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.FilterChain
-import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+@Component
+class UserAuthFilter
 
-class UserAuthFilter(
-        private val userService: UserService,
-        matcher: RequestMatcher)
+    constructor(
+        @Autowired val tokenService: TokenService,
+        @Autowired val userService: UserService,
+        @Autowired val authEntryPoint: AuthEntryPoint)
 
-    : AbstractAuthenticationProcessingFilter(matcher) {
+    : OncePerRequestFilter() {
 
-    override fun attemptAuthentication(request: HttpServletRequest, response: HttpServletResponse): Authentication {
-        val token = request.getHeader(AUTHORIZATION)
-        val user = userService.findByToken(token)
-        val auth = UsernamePasswordAuthenticationToken(user, token)
-        return authenticationManager.authenticate(auth)
-    }
+    override fun doFilterInternal(request: HttpServletRequest,
+                                  response: HttpServletResponse,
+                                  chain: FilterChain) {
 
-    @Throws(IOException::class, ServletException::class)
-    override fun successfulAuthentication(
-            request: HttpServletRequest,
-            response: HttpServletResponse,
-            chain: FilterChain,
-            authResult: Authentication) {
 
-        super.successfulAuthentication(request, response, chain, authResult)
-        chain.doFilter(request, response)
-    }
+        val token = request.getHeader("Authorization");
+        var username: String? = null
 
-    override fun unsuccessfulAuthentication(request: HttpServletRequest,
-                                            response: HttpServletResponse,
-                                            failed: AuthenticationException) {
+        if (token != null) {
+            try {
+                username = tokenService.getUsername(token)
+            } catch (e: IllegalArgumentException) {
+                println("Unable to get JWT Token");
+            } catch (e: ExpiredJwtException) {
+                println("JWT Token has expired");
+            }
+        }
 
-        throw InvalidUserException("Unsuccessful authentication.")
+        if (username != null && SecurityContextHolder.getContext().authentication == null) {
+
+            val user = userService.findByToken(token)
+
+            if (user != null) {
+                val springToken = UsernamePasswordAuthenticationToken(user, null, user.authorities)
+                springToken.details = WebAuthenticationDetailsSource().buildDetails(request);
+
+                SecurityContextHolder.getContext().authentication = springToken
+            }
+        }
+
+        chain.doFilter(request, response);
     }
 }
